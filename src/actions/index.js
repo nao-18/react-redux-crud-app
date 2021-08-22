546fb06042f45fb4a7e8f275f461d94745f5e740
nao-18 <kyahyo17@yahoo.co.jp>
2021-08-23 08:54:49 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+//actionを定義
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+
+export const increment = () => ({ type: INCREMENT });
+export const decrement = () => ({ type: DECREMENT });
