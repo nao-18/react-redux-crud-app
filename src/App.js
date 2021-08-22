1e676acffef0ee7939e14f2cdf120d7a8a7db4fd
nao-18 <kyahyo17@yahoo.co.jp>
2021-08-23 06:44:33 +0900

    Learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -4,11 +4,34 @@ function App() {
   /**
    * jsxの方が直感的に記述できる
    */
+  // const greeting = 'Hi, Tom!';
+  // //jsxではclassは"className"を使う
+  // const dom = <h1 className="foo">{greeting}</h1>;
+  // return dom;
 
-  // return <h1>Hello, World!</h1>;
+  //onClick
+  return (
+    // <div>
+    //   <label htmlFor="foo">foo</label>
+    //   <input
+    //     type="text"
+    //     onClick={() => {
+    //       console.log('I am clicked.');
+    //     }}
+    //   />
+    // </div>
 
-  //jsで記述した場合
-  return React.createElement('div', null, 'Hello, World!');
+    //外側のタグを表示したくない場合は"React.Fragment"
+    <React.Fragment>
+      <label htmlFor="foo">foo</label>
+      <input
+        type="text"
+        onClick={() => {
+          console.log('I am clicked.');
+        }}
+      />
+    </React.Fragment>
+  );
 }
 
 export default App;
