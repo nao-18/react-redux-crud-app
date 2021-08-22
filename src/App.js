f4e468c3b6e7a267dee401c5ce9854958601a173
nao-18 <kyahyo17@yahoo.co.jp>
2021-08-23 08:33:44 +0900

    create state

--- src/App.js
+++ src/App.js
@@ -1,48 +1,42 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
 //Functionコンポーネント
-const App = () => {
-  const profiles = [
-    {
-      name: 'Taro',
-      age: 20,
-    },
-    {
-      name: 'Hanako',
-      age: 30,
-    },
-    {
-      name: 'NoName',
-    },
-  ];
-  return (
-    <div>
-      {profiles.map((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index} />;
-      })}
-    </div>
-  );
-};
+const App = () => <Counter />;
 
-const User = (props) => {
-  return (
-    <div>
-      Hi, I am {props.name}, and {props.age} years old!
-    </div>
-  );
-};
+class Counter extends Component {
+  //親からpropsを受け取る
+  constructor(props) {
+    //親クラスでpropsを初期化する
+    super(props);
+    this.state = { count: 0 };
+  }
 
-//PropTypes
-User.propTypes = {
-  name: PropTypes.string,
-  //必須化(isRequired)
-  age: PropTypes.number.isRequired,
-};
+  //メンバ変数
+  hundlePlusButton = () => {
+    //setStateによってstateを更新した際に、再度renderが呼ばれる
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
 
-//defaultProps
-User.defaultProps = {
-  age: 1,
-};
+  hundleMinusButton = () => {
+    const count = this.state.count;
+    if (count > 0) {
+      this.setState({
+        count: count - 1,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.hundlePlusButton}>+1</button>
+        <button onClick={this.hundleMinusButton}>-1</button>
+      </React.Fragment>
+    );
+  }
+}
 
 export default App;
