a30113e6a1e68d6357e3b2da3636c16e6bd43c89
nao-18 <kyahyo17@yahoo.co.jp>
2021-08-23 06:15:59 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
+
 function App() {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  );
+  /**
+   * jsxの方が直感的に記述できる
+   */
+
+  // return <h1>Hello, World!</h1>;
+
+  //jsで記述した場合
+  return React.createElement('div', null, 'Hello, World!');
 }
 
 export default App;
