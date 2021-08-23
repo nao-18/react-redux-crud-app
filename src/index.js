1aaf255f833848d64e77724b461cce27adf41565
nao-18 <kyahyo17@yahoo.co.jp>
2021-08-24 06:55:29 +0900

    fix typo and error

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createCount } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import reducer from './reducers';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-const store = createCount(reducer);
+const store = createStore(reducer);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,13 +1,16 @@
-import { INCREMENT, DECRIMENT } from '../actions';
+import { INCREMENT, DECREMENT } from '../actions';
 
 const initialState = { value: 0 };
-export default (state = initialState, action) => {
+
+const switchEvent = (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
       return { value: state.value + 1 };
-    case DECRIMENT:
+    case DECREMENT:
       return { value: state.value - 1 };
     default:
       return state;
   }
 };
+
+export default switchEvent;
