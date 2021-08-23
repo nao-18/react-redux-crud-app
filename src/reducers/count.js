fe8f74f0216c5e794390fdf922eaaa1c0b86dc87
nao-18 <kyahyo17@yahoo.co.jp>
2021-08-24 06:16:34 +0900

    implement reducer

---
+++ src/reducers/count.js
@@ -0,0 +1,13 @@
+import { INCREMENT, DECRIMENT } from '../actions';
+
+const initialState = { value: 0 };
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { value: state.value + 1 };
+    case DECRIMENT:
+      return { value: state.value - 1 };
+    default:
+      return state;
+  }
+};
