932136985d046daecb79c4c7a4bd64a926f1e40c
nao-18 <kyahyo17@yahoo.co.jp>
2021-08-24 06:29:43 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createCount } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import reducer from './reducers';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createCount(reducer);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
